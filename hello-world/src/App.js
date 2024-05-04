2b157c1c4a7e78967bfdf5bef03a7d9f348675c0
Yogeshzade-git <ymzade2019@gmail.com>
2024-05-04 14:39:18 +0530

    app created

---
+++ hello-world/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello This App Deployed in Paas Environment</h1>
+    </div>
+  );
+}
+
+export default App;
